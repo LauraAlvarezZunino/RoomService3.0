3ec519378c75982cd631865448d6ecb0142bacca
LauraAlvarezZunino <lauraalvarezzunino@gmail.com>
2025-10-06 15:32:27 -0300

    modularize codigo de index

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -7,8 +7,8 @@ class Database {
     private function __construct() {
         $host = 'localhost'; 
         $db   = 'room_service'; 
-        $user = 'maria';
-        $pass = '1234';
+        $user = 'root';
+        $pass = 'admin123';
         $charset = 'utf8mb4';
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
